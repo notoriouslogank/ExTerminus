b57f486b1c64635213acc8b592a78636ec8b5d7e
notoriouslogank <logan.aker1989@gmail.com>
2025-08-10 00:23:10 -0400

    chore(config, logger): deleted deprecated files (moved to utils/)

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-insecure-change-me")
-    SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SAMESITE = "Lax"
-    SESSION_COOKIE_SECURE = bool(int(os.environ.get("SESSION_COOKIE_SECURE", "0"))) # change to 1 in production
-    PERMANENT_SESSION_LIFETIME = 60 * 60 * 12 # 12h
